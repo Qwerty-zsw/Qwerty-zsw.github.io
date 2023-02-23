1975292f1ca9361aafa5e5eaa0f2176bb62d5f60
Masoud Hajiloo <masoud1223z@gmail.com>
2023-02-23 18:00:02 +0330

    r-r-d added

--- src/MainHead/MainHead.jsx
+++ src/MainHead/MainHead.jsx
@@ -10,16 +10,16 @@ const MainHead = () => {
   return (
     <>
       <Navbar bg="transparent" expand="sm">
-        <Container fluid>
-          <Navbar.Brand className="cusLog2" href="#">MasterGame</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
+        <Container fluid className="text-white">
+          <Navbar.Brand className="cusLog2 text-white" href="#">MasterGame</Navbar.Brand>
+          <Navbar.Toggle className="text-white" aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Form className="d-flex inpStyle position-relative mx-auto">
-                <a className="SearchIcon text-white" href="#"><IoSearchCircleOutline className="fs-2 position-absolute icon" /></a>
+                <a className="SearchIcon text-black" href="#"><IoSearchCircleOutline className="fs-2 position-absolute icon" /></a>
                 <Form.Control
                   type="search"
                   placeholder="دنبال چه چیزی هستید؟"
-                  className="d-flex bg-transparent rounded-4 pe-5 py-2"
+                  className="d-flex rounded-4 pe-5 py-2"
                   aria-label="Search"
                 />
             </Form>
@@ -28,12 +28,12 @@ const MainHead = () => {
               style={{ maxHeight: "300px" }}
               navbarScroll
             >
-              <NavDropdown title="آموزش ها" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">MMORPG</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">FPS</NavDropdown.Item>
-                <NavDropdown.Item href="#action5">OFFLINE</NavDropdown.Item>
+              <NavDropdown className="fw-bold" title="آموزش ها" id="navbarScrollingDropdown">
+                <NavDropdown.Item className="fw-bold" href="#action3">MMORPG</NavDropdown.Item>
+                <NavDropdown.Item className="fw-bold" href="#action4">FPS</NavDropdown.Item>
+                <NavDropdown.Item className="fw-bold" href="#action5">OFFLINE</NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link className="bg-info text-white cusLog" href="#action1">ورود</Nav.Link>
+              <Nav.Link className="bg-white text-black fw-bold cusLog" href="#action1">ورود</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
