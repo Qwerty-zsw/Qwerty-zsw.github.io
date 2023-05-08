43d1067df3f31fdc217ee7c00a99af5fbcef93f3
Masoud Hajiloo <masoud1223z@gmail.com>
2023-05-08 20:51:00 +0430

    privRoute on "آموزش ها"

--- src/MainHead/MainHead.jsx
+++ src/MainHead/MainHead.jsx
@@ -1,27 +1,21 @@
 import "./MainHead.css";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { IoSearchCircleOutline } from "react-icons/io5";
 import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
 import { MdMenu } from "react-icons/md";
 import { BiMenuAltRight } from "react-icons/bi";
 import { useState } from "react";
+import { IoSearchCircleOutline } from "react-icons/io5";
+import Form from "react-bootstrap/Form";
+import { showLogin } from "../util";
 
 const MainHead = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [Max, setMax] = useState(false);
-
-  const clickHandler = () => {
-    setIsOpen(!isOpen);
-  };
-  
-  const maxHandler = () => {
-    setMax(!Max);
-  };
+  const isUserLogin = showLogin("Masoud");
 
   return (
     <>
@@ -30,7 +24,7 @@ const MainHead = () => {
           <Navbar.Toggle
             className="text-light fs-1"
             aria-controls="navbarScroll"
-            onClick={clickHandler}
+            onClick={() => setIsOpen(!isOpen)}
           >
             {isOpen ? <BiMenuAltRight /> : <MdMenu />}
           </Navbar.Toggle>
@@ -44,35 +38,47 @@ const MainHead = () => {
             </Navbar.Brand>
           </Link>
           <Navbar.Collapse className="body-collapse" id="navbarScroll">
-          
             <div className="w-100 d-flex justify-content-center">
-            <Form className={"position-relative fixedCenter"}>
-              <Form.Control
-                type="search"
-                placeholder="دنبال چه چیزی هستید؟"
-                className="rounded-5 py-2 ps-5"
-                aria-label="Search"
-              />
-              <IoSearchCircleOutline className="fs-2 icon search-icon text-black" />
-            </Form>
+              <Form className={"position-relative fixedCenter"}>
+                <Form.Control
+                  type="search"
+                  placeholder="دنبال چه چیزی هستید؟"
+                  className="rounded-5 py-2 ps-5"
+                  aria-label="Search"
+                />
+                <IoSearchCircleOutline className="fs-2 icon search-icon text-black" />
+              </Form>
             </div>
             <Nav
-              className={`my-2 my-lg-0 position-relative cusMax ${Max ? "cusOpen" : ""}`}
+              className={`my-2 my-lg-0 position-relative cusMax ${
+                Max ? "cusOpen" : ""
+              }`}
               navbarScroll
             >
               <NavDropdown
-                onClick={maxHandler}
-                className="fw-bold text-center"
+                onClick={() => setMax(!Max)}
+                className={`fw-bold text-center ${
+                  isUserLogin ? "" : "invisible"
+                }`}
                 title="آموزش ها"
                 id="navbarScrollingDropdown"
               >
-                <NavDropdown.Item className="fw-bold" href="#">
+                <NavDropdown.Item
+                  className="fw-bold rounded-5 text-center"
+                  href="#"
+                >
                   MMORPG
                 </NavDropdown.Item>
-                <NavDropdown.Item className="fw-bold" href="#">
+                <NavDropdown.Item
+                  className="fw-bold rounded-5 text-center"
+                  href="#"
+                >
                   FPS
                 </NavDropdown.Item>
-                <NavDropdown.Item className="fw-bold" href="#">
+                <NavDropdown.Item
+                  className="fw-bold rounded-5 text-center"
+                  href="#"
+                >
                   OFFLINE
                 </NavDropdown.Item>
               </NavDropdown>
