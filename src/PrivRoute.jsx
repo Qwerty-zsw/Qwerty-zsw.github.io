ae5aaec69812dc677ea98a7806aa127e632307c1
Masoud Hajiloo <masoud1223z@gmail.com>
2023-05-12 00:32:37 +0430

    Outlet added / new meth note added

--- src/PrivRoute.jsx
+++ src/PrivRoute.jsx
@@ -1,13 +1,18 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { showLogin } from "./util";
 
-const PrivRoute = ({ children }) => {
+const PrivRoute = ({children}) => {
   const isUserLogin = showLogin("Masoud");
 
-  return <div>{isUserLogin ? { children } : <Navigate to={"/ورود"} />}</div>;
+  return <div>{isUserLogin ? <Outlet /> : <Navigate to={"/ورود"} />}</div>;
 };
-// you have to put this component to elements component in "routes.jsx"
+// meth1: you have to put this component to elements component in "routes.jsx"
 // => e: this> {children} <this
 // children = other component details
+
+// meth2: render THIS element in "routes.jsx with /* path
+// add ,children property with array value
+// the continuation is the same as the routes method but path without "/"
+// like this=> "panel"
 
 export default PrivRoute;
