687199ef7b007be33a3945b7fb094960b8864218
Masoud Hajiloo <masoud1223z@gmail.com>
2023-06-21 18:32:02 +0330

    scroll to top added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useRoutes } from "react-router-dom";
 import routes from "./routes";
+import { Element } from "react-scroll";
+import { animateScroll as scroll } from 'react-scroll';
 
 const App = () => {
   let router = useRoutes(routes);
+  scroll.scrollTo(0);
 
   return (
-    <div className="w-100 h-100">
-      {router}
-    </div>
+    <Element name="top" className="element w-100 h-100">
+      <div className="w-100 h-100">{router}</div>
+    </Element>
   );
 };
 
